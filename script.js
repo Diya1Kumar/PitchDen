360e51a330664e883d55ad5ffcf7307fe1f59f00
Diya Kumar <diyadk11042005@gmail.com>
2025-02-03 14:57:26 +0530

    commit

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-const gallery = document.querySelector(".gallery");
-const images = document.querySelectorAll(".card-img-top");
-let index = 0;
-const imagesPerView = 3; // Number of images visible at once
-
-function updateGallery() {
-    const imageWidth = images[0].clientWidth + 10; // Include margin
-    gallery.style.transform = `translateX(-${index * imageWidth}px)`;
+function showMenu() {
+    document.getElementById("navLinks").classList.add("nav-active");
+    document.getElementById("menu-icon").style.display = "none";
+    document.getElementById("close-icon").style.display = "block";
 }
 
-function nextSlide() {
-    if (index < images.length - imagesPerView) {
-        index++;
-    } else {
-        index = 0; // Reset to the start when reaching the end
-    }
-    updateGallery();
-}
-
-function prevSlide() {
-    if (index > 0) {
-        index--;
-    } else {
-        index = images.length - imagesPerView; // Loop to the end
-    }
-    updateGallery();
+function hideMenu() {
+    document.getElementById("navLinks").classList.remove("nav-active");
+    document.getElementById("menu-icon").style.display = "block";
+    document.getElementById("close-icon").style.display = "none";
 }
